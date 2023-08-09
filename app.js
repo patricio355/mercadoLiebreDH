b77ba16b17938a0f5d1a7e38e2d8735eb651f7bf
patricio355 <patricioquiroga35@gmail.com>
2023-08-09 10:22:32 -0300

    correcciones

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.get('/' , (req,res)=> {
     res.sendFile(path.join(__dirname,'src/views/home.html'))
 })
 
+app.get('/register' , (req,res)=> {
+    res.sendFile(path.join(__dirname,'src/views/register.html'))
+})
+
+app.get('/login' , (req,res)=> {
+    res.sendFile(path.join(__dirname,'src/views/login.html'))
+})
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log("servidor iniciado"))
